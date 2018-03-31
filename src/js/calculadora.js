cd74b5a4c8c7fb80f47e3e166ad40b409dcdb274
fabio guelfi <fabioguelfiunix@gmail.com>
2018-03-31 14:13:34 -0300

    refatoracao calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,47 +1,59 @@
 let Calculadora = {
-    adicionar: (num1, num2) => {
 
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
+    adicionar: (num1, num2) => {
+        return Calculadora.calcular(num1, num2, Calculadora.ADICAO)
+    },
+    subtrair: (num1, num2) => {
+        return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO)
+    },
+    dividir: (num1, num2) => {
+        if (num2 == 0) {
+            return 'Erro'
         }
+        return Calculadora.calcular(num1, num2, Calculadora.DIVISAO)
+    },
+    multiplicar: (num1, num2) => {
+        return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO)
+    },
+
+    calcular: (num1, num2, operacao) => {
+
+        let resultado = 0
 
         num1 = parseFloat(num1)
         num2 = parseFloat(num2)
 
-        return num1 + num2
-
-    },
-    subtrair: (num1, num2) => {
-
         if (isNaN(num1) || isNaN(num2)) {
             return 0
         }
 
-        return num1 - num2
-
-    },
-    dividir: (num1, num2) => {
-
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
+        switch (operacao) {
+            case Calculadora.ADICAO:
+                resultado = num1 + num2
+                break
+            case Calculadora.SUBTRACAO:
+                resultado = num1 - num2
+                break
+            case Calculadora.DIVISAO:
+                resultado = num1 / num2
+                break
+            case Calculadora.MULTIPLICACAO:
+                resultado = num1 * num2;
+                break
+            default:
+                resultado = 0
+                break
         }
 
-        if (num2 == 0) {
-            return 'Erro'
-        }
+        return resultado
 
-        return num1 / num2
+    }
 
-    },
-    multiplicar: (num1, num2) => {
-
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-
-        return num1 * num2
-
-    },
 }
 
 // usado na func 'require' do NodeJs
